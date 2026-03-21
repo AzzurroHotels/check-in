badfcef359539de7cf178361582bb311fd13e929
pranavcl <pranavcl@outlook.com>
2026-03-21 11:23:31 +0530

    Update pm2 to use python

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [{
     name: 'check-in',
     cwd: '/var/www/check-in',
+    interpreter: './venv/bin/python3',
     script: './venv/bin/gunicorn',
     args: '--workers 3 --bind localhost:5005 app:app',
     autorestart: true,
